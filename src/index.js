29e3fe4aaa6849f4134dcc24c87c96cd946e2bf2
DESKTOP-DOKS1I1\user <a@a.a>
2024-02-27 19:10:44 +0200

    initial

--- src/index.js
+++ src/index.js
@@ -1,2 +1,64 @@
-// index.js
-// This is the main entry point of our application
+const express = require('express');
+const { ApolloServer, gql } = require('apollo-server-express');
+require('dotenv').config();
+
+const db = require('./db');
+const models = require('./models');
+
+// Запускаем сервер на порте, указанном в файле .env, или на порте 4000
+const port = process.env.PORT || 4000;
+// Сохраняем значение DB_HOST в виде переменной
+const DB_HOST = process.env.DB_HOST;
+
+// Строим схему, используя язык схем GraphQL
+const typeDefs = gql`
+  type Note {
+    id: ID!
+    content: String!
+    author: String!
+  }
+  type Query {
+    hello: String
+    notes: [Note!]!
+    note(id: ID!): Note!
+  }
+  type Mutation {
+    newNote(content: String!): Note!
+  }
+`;
+
+// Предоставляем функцию распознавания для полей схемы
+const resolvers = {
+  Query: {
+    hello: () => 'Hello world!',
+    notes: async () => {
+      return await models.Note.find();
+    },
+    note: async (parent, args) => {
+      return await models.Note.findById(args.id);
+    },
+  },
+  Mutation: {
+    newNote: async (parent, args) => {
+      return await models.Note.create({
+        content: args.content,
+        author: 'Adam Scott'
+      });
+    }
+  }
+}; 
+
+const app = express();
+// Подключаем БД
+db.connect(DB_HOST);
+
+// Настраиваем Apollo Server
+const server = new ApolloServer({ typeDefs, resolvers });
+
+// Применяем промежуточное ПО Apollo GraphQL и указываем путь к /api
+server.applyMiddleware({ app, path: '/api' });
+app.listen({ port }, () =>
+  console.log(
+  `GraphQL Server running at http://localhost:${port}${server.graphqlPath}`
+  )
+)
